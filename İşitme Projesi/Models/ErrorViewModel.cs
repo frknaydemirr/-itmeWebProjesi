b48d38816d7ae40e40f92ae07b5d02d08fbd3a0c
Furkan Aydemir <furkanaydemir12@hotmail.com>
2025-04-03 23:36:08 +0300

    first commit

---
+++ İşitme Projesi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace İşitme_Projesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
